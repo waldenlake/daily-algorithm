fc5d324e1ba2de59cda08cb41bdfe128c25fe87a
lile <lile@haodelian.com>
2020-01-04 15:00:29 +0800

    init project

---
+++ src/main/java/com/walden/dailyalgorithm/DailyAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.walden.dailyalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyAlgorithmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/walden/dailyalgorithm/DailyAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walden.dailyalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
